059f7c532e35302e46d3a7041ee1b741a539c6a0
iamliujingjiu <1058760330@qq.com>
2021-04-29 11:30:21 +0800

    修改了拷贝的代码 添加了map和set以及单独传一个参数时返回拷贝值

--- shallowCopy.js
+++ shallowCopy.js
@@ -1,77 +1,90 @@
-var init = false, 
-_isArray = Array.isArray,
-_ownKeys = Reflect && Reflect.ownKeys,
-_getOwnPropertySymbols = Object.getOwnPropertySymbols,
-// _getOwnPropertyNames = Object.getOwnPropertyNames,
-_keys = Object.keys,
-_hasOwnProperty = Object.prototype.hasOwnProperty,
-_defineProperty = Object.defineProperty,
-_getOwnPropertyDescriptor = Object.getOwnPropertyDescriptor;
+
+let init = false;
 
 function initShallowCopy(){
     if(init){
         return;
     }
+
     init = true;
-    _defineProperty(Object,'shallowCopy',{
+
+    Object.defineProperty(Object, 'shallowCopy', {
         value : function shallowCopy(){
-            var length = arguments.length,
-            target,copy,copykeys,
-            i = 1,j,n,keysLen = 0,
-            copyLen,key;
-        
-            target = arguments[0];
-        
+    
+            let target = arguments[0];
+    
+            let i = 1;
+    
+            let length = arguments.length;
+    
+           
             if(typeof target === 'undefined' || (!target && typeof target === 'object')){
                 throw Error('Cannot convert undefined or null to object');
-            }else if(typeof target === 'boolean'){
-                return Boolean(target);
-            }else if(typeof target === 'number'){
-                return Number(target);
-            }else if(typeof target === 'bigint'){
-                return BigInt(target);
-            }else if(typeof target === 'symbol'){
-                return target;
-            }else if(typeof target === 'string'){
-                target = new String(target);
             }
             
+            if(typeof target !== 'object' && typeof target !== 'function' && typeof target !== 'string'){
+                return Object(target);
+            }
+            
+            if(typeof target === 'string'){
+                target = Object(target);
+            }
+    
+            if(length === 1){
+                i = 0;
+    
+                if(Array.isArray(target)){
+                    target = [];
+                }else if(target instanceof Map){
+                    target = new Map();
+                }else if(target instanceof Set){
+                    target = new Set();
+                }else if((typeof target === 'object' && null !== target) || typeof target === 'function'){
+                    target = {};
+                }
+            }
+    
             while(i < length){
-                copy = arguments[i++];
-                if(typeof copy === 'string'){
-                    copy = _changeStringtoArray(copy);
-                }
-                if(typeof copy !== 'object' || !copy){
+                
+                let copy = arguments[i++];
+    
+                if(typeof copy === 'undefined' || (!copy && typeof copy === 'object')){
                     continue;
-                }else if(_isArray(copy)){
-                    copykeys = new Array(copyLen = copy.length);
-                    for(n = 0;n<copyLen;n++){
-                        copykeys[n] = n;
-                    }
-                }else{
-                    if(_ownKeys){
-                        copykeys = _getCopykeys(_ownKeys,copy);
-                    }else if(_getOwnPropertySymbols){
-                        copykeys = _getCopykeys(_getOwnPropertySymbols,copy);
-                        copykeys = copykeys.concat(_keys(copy));
-                    }else if(_keys){
-                        copykeys = _keys(copy);
-                    }else{
-                        for(key in copy){
-                            if(_hasOwnProperty.call(copy,key)){
-                                target[key] = copy[key];
-                            }
-                        }
-                        continue;
-                    }
                 }
-                for(j = 0,keysLen = copykeys.length;j<keysLen;j++){
-                    key = copykeys[j];
-                    if(_hasOwnProperty.call(copy,key)){
+                if(typeof copy === 'string'){
+                    copy = changeStringtoArray(copy);
+                }
+    
+                //array map set也属于object
+                if((typeof target === 'object' && null !== target) || typeof target === 'function'){
+                    let keys = traverseObj(copy);
+                    for(let j = 0, len = keys.length; j < len; j++){
+                        let key = keys[j];
                         target[key] = copy[key];
                     }
                 }
+    
+                // 1.两个都是map
+                if(copy instanceof Map && target instanceof Map){
+                    for(let [key, value] of copy){
+                        target.set(key, value);
+                    }
+                }
+    
+                // 2.两个都是set
+                if(copy instanceof Set && target instanceof Set){
+                    for(let value of copy){
+                        target.add(value);
+                    }
+                }
+    
+                if(Array.isArray(copy)){
+                    for(let j = 0, len = copy.length; j < len; j++){
+                        target[j] = copy[j];
+                    }
+                }
             }
+    
             return target;
         },
         writable: false,
@@ -80,19 +93,19 @@ function initShallowCopy(){
     })
 }
 
-function _changeStringtoArray(str){
+function traverseObj(val){
+    let symbols = Object.getOwnPropertySymbols ? Object.getOwnPropertySymbols(val) : [];
+    let keys = Object.keys ? Object.keys(val) : [];
+    return Array.prototype.concat.call(keys, symbols);
+}
+
+function changeStringtoArray(str){
     let length = str.length || 0;
     let strArr = new Array(length);
-    for(let j = 0;j<length;j++){
+    for(let j = 0; j < length; j++){
         strArr[j] = str[j];
     }
     return strArr;
-}
-
-function _getCopykeys(func,obj){
-    return func(obj).filter(function(value){
-        return _getOwnPropertyDescriptor(obj,value).enumerable;
-    });
 }
 
 module.exports = {
